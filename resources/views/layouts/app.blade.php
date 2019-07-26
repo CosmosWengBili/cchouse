4cccf9da818b6f37cd830d2ae55da2af46d42f5e
johnliu <johnliutw@hotmail.com>
2019-07-26 12:19:55 +0800

    static file path from normal value to assest path helper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>CCHouse</title>
         <!-- base:css -->
-        <link rel="stylesheet" href="vendors/typicons/font/typicons.css">
-        <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+        
+        <link rel="stylesheet" href={{ asset('vendors/typicons/font/typicons.css') }}>
+        <link rel="stylesheet" href={{ asset('vendors/css/vendor.bundle.base.css') }}>
         <!-- endinject -->
         <!-- plugin css for this page -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href={{ asset('css/datatables.css') }}>
         <!-- End plugin css for this page -->
         <!-- inject:css -->
-        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href={{ asset('css/app.css') }}>
         <!-- endinject -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
     </head>
     <body>
         <div class="container-scroller">
@@ -26,19 +33,23 @@
         </div>
         <!-- container-scroller -->
         <!-- base:js -->
-        <script src="vendors/js/vendor.bundle.base.js"></script>
+        <script src={{ asset('vendors/js/vendor.bundle.base.js') }}></script>
         <!-- endinject -->
         <!-- Plugin js for this page-->
         <!-- End plugin js for this page-->
         <!-- inject:js -->
-        <script src="js/manifest.js"></script>
-        <script src="js/vendor.js"></script>
+        
+        <script src={{ asset('js/manifest.js') }}></script>
+        <script src={{ asset('js/vendor.js') }}></script>
         <!-- endinject -->
         <!-- plugin js for this page -->
-        <script src="vendors/progressbar.js/progressbar.min.js"></script>
-        <script src="vendors/chart.js/Chart.min.js"></script>
-        <script src="vendors/flot/jquery.flot.js"></script>
-        <script src="vendors/flot/curvedLines.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+        <script src="https://kit.fontawesome.com/3b5743464b.js"></script>
         <!-- End plugin js for this page -->
     </body>
 </html>

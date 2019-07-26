a722a46dcb3cafc219f10bcb457991e452739e64
johnliu <johnliutw@hotmail.com>
2019-07-26 17:15:13 +0800

    DataTable original code

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,5 +16,8 @@ mix.scripts(['resources/vendor/dashboard.js',
              'resources/vendor/off-canvas.js',
              'resources/vendor/settings.js',
              'resources/vendor/template.js',
-             'resources/vendor/todolist.js'], 'public/js/vendor.js')
-    .sass('resources/sass/app.scss', 'public/css');
+             'resources/vendor/todolist.js',
+             'resources/vendor/datatables.js',
+             'resources/vendor/datatables-module.js'], 'public/js/vendor.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/datatables.scss', 'public/css/datatables.css');

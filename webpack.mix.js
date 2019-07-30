ccb99c672914e009f03f3d818f0d293e654c793f
johnliu <johnliutw@hotmail.com>
2019-07-30 18:31:05 +0800

    Add library dependency

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,6 +18,10 @@ mix.scripts(['resources/vendor/dashboard.js',
              'resources/vendor/template.js',
              'resources/vendor/todolist.js',
              'resources/vendor/datatables.js',
-             'resources/vendor/datatables-module.js'], 'public/js/vendor.js')
+             'resources/vendor/datatables-module.js',
+             'node_modules/selectize/dist/js/standalone/selectize.min.js',
+             'node_modules/selectize/dist/js/selectize.min.js'], 'public/js/vendor.js')
+    .scripts('resources/js/global.js', 'public/js/app.js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/datatables.scss', 'public/css/datatables.css');
+    .sass('resources/sass/datatables.scss', 'public/css/datatables.css')
+    .less('node_modules/selectize/dist/less/selectize.less', 'public/css/vendor.css');

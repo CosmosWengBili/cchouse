226efc5896685d2fcf98cc6755fb754d410ba217
David Liu <msglight4874@gmail.com>
2019-08-02 19:49:42 +0800

    Register auditing service provider

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        OwenIt\Auditing\AuditingServiceProvider::class,
 
         /*
          * Application Service Providers...

2d0539e65e247acaf897885fded006e347c03352
ben <huang831206@gmail.com>
2019-08-03 14:42:32 +0800

    separate landlord blade files

---
+++ resources/views/landlords/form.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <form action="{{$action}}" method="POST">
+        @csrf
+        @method($method)
+
+        @foreach ( $data as $attribute => $value )
+            {{-- handle your own type and empty policy --}}
+            @continue(is_array($value))
+            @if ($method === 'PUT')
+                <span> {{ $attribute }} : </span>
+                <input type="text" name="{{$attribute}}" value="{{$value}}">
+            @else
+                <span> {{ $value }} : </span>
+                <input type="text" name="{{$value}}">
+            @endif
+        @endforeach
+
+        <button type="submit">Submit</button>
+    </form>
+</div>
+@endsection

93d205e3f18fcb1ecb1b7815c5ab67963cc592ad
ben <huang831206@gmail.com>
2019-08-09 15:30:50 +0800

    implement basic room CRUD

---
+++ resources/views/rooms/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            {{-- for showing multiple types of entries returned --}}
+            @foreach ( $data as $type => $entries)
+                
+        
+                @include('rooms.table', ['objects' => $entries, 'layer' => $type])
+
+            @endforeach
+
+        </div>
+
+    </div>
+</div>
+@endsection

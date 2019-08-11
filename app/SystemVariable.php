6cfe3d8058691514f849b165133e688cb18de33c
David Liu <msglight4874@gmail.com>
2019-08-12 00:05:19 +0800

    Add VARIABLES const to `SystemVariable` model

--- app/SystemVariable.php
+++ app/SystemVariable.php
@@ -3,8 +3,49 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class SystemVariable extends Model
 {
-    //
+    const OPTIONS = [
+        ['name' => '維修清潔狀態通知天數', 'code' => 'MaintenanceNotifyRequiredDays', 'type' => 'integer', 'defaultValue' => 10],
+    ];
+
+    public static function get(string $code) {
+        $option = self::searchArray($code, 'code', self::OPTIONS);
+        if (is_null($option)) {
+            return null;
+        }
+
+        $variable = self::where('code', $code)->first();
+        if (is_null($variable)) {
+            return $option['defaultValue'];
+        }
+
+        return self::castValue($option['type'], $variable->value);
+    }
+
+    private function castValue(string $type, string $valueString) {
+        switch ($type) {
+            case 'integer':
+                return intval($valueString);
+            case 'float':
+                return floatval($valueString);
+            case 'boolean':
+                return boolval($valueString);
+            case 'datetime':
+                return Carbon::parse($valueString);
+            default:
+                return $valueString;
+        }
+    }
+
+    private static function searchArray($value, $key, $array) {
+        foreach ($array as $k => $val) {
+            if ($val[$key] == $value) {
+                return $k;
+            }
+        }
+        return null;
+    }
 }

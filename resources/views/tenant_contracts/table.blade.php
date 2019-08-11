5df58ec8680b953810bedde257ff25cdcdf0fabf
David Liu <msglight4874@gmail.com>
2019-08-12 00:46:14 +0800

    Remove `Route::has` check

--- resources/views/tenant_contracts/table.blade.php
+++ resources/views/tenant_contracts/table.blade.php
@@ -45,18 +45,12 @@
                             {{-- render all attributes --}}
                             @foreach($object as $key => $value)
                                 {{-- an even nested resource array --}}
-                                <td> {{ $value }}</td>
+                                <td> {{ print_r($value) }}</td>
                             @endforeach
                             <td>
-                                @if(Route::has(Str::camel($layer) . '.show'))
-                                    <a class="btn btn-success" href="{{ route( Str::camel($layer) . '.show', $object['id']) }}">查看</a>
-                                @endif
-                                @if(Route::has(Str::camel($layer) . '.edit'))
-                                    <a class="btn btn-primary" href="{{ route( Str::camel($layer) . '.edit', $object['id']) }}">編輯</a>
-                                @endif
-                                @if(Route::has(Str::camel($layer) . '.destroy'))
-                                    <a class="btn btn-danger jquery-postback" data-method="delete" href="{{ route( Str::camel($layer) . '.destroy', $object['id']) }}">刪除</a>
-                                @endif
+                                <a class="btn btn-success" href="{{ route( Str::camel(Str::plural($layer)) . '.show', $object['id']) }}">查看</a>
+                                <a class="btn btn-primary" href="{{ route( Str::camel(Str::plural($layer)) . '.edit', $object['id']) }}">編輯</a>
+                                <a class="btn btn-danger jquery-postback" data-method="delete" href="{{ route( Str::camel(Str::plural($layer)) . '.destroy', $object['id']) }}">刪除</a>
                             </td>
                         </tr>
                     @endforeach

75de9b2812851d678b6fd5882482c7d77159d457
ben <huang831206@gmail.com>
2019-08-12 13:10:21 +0800

    use transaction on create of tenant contract

--- app/Services/TenantContractService.php
+++ app/Services/TenantContractService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\TenantContract;
 use App\TenantPayment;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 use App\Services\PeriodService;
 
@@ -18,34 +19,39 @@ class TenantContractService{
     // 1. create tenant contract
     // 2. generate payments
     public function create($data, $payments = []) {
-        // create tenant contract
-        $tenantContractId = TenantContract::insertGetId($data);
-        $tenantContract = TenantContract::find($tenantContractId);
-        
-        // by default, each tenant contract has one rent payment per month
-        $payments[] = [
-            'subject'       => '租金',
-            'period'        => '月',
-            'amount'        => $tenantContract->rent,
-            'collected_by'  => '公司',
-        ];
 
-        // generates all kinds of payments
-        foreach ($payments as $payment) {
-            // get the dates that this payment shoud be collected
-            // make a payment instance for it
-            $futurePayments = $this->periodService->every($payment['period'], $tenantContract->contract_start, $tenantContract->contract_end, function ($date) use ($payment) {
-                return TenantPayment::make([
-                    'subject'       => $payment['subject'],
-                    'due_time'      => $date,
-                    'amount'        => $payment['amount'],
-                    'collected_by'  => $payment['collected_by'],
-                ]);
-            });
+        $tenantContract = DB::transaction(function () use ($data, $payments) {
+            // create tenant contract
+            $tenantContractId = TenantContract::insertGetId($data);
+            $tenantContract = TenantContract::find($tenantContractId);
             
-            // link these payments to the newly created tenant contract
-            $tenantContract->tenantPayments()->saveMany($futurePayments);
-        }
+            // by default, each tenant contract has one rent payment per month
+            $payments[] = [
+                'subject'       => '租金',
+                'period'        => '月',
+                'amount'        => $tenantContract->rent,
+                'collected_by'  => '公司',
+            ];
+    
+            // generates all kinds of payments
+            foreach ($payments as $payment) {
+                // get the dates that this payment shoud be collected
+                // make a payment instance for it
+                $futurePayments = $this->periodService->every($payment['period'], $tenantContract->contract_start, $tenantContract->contract_end, function ($date) use ($payment) {
+                    return TenantPayment::make([
+                        'subject'       => $payment['subject'],
+                        'due_time'      => $date,
+                        'amount'        => $payment['amount'],
+                        'collected_by'  => $payment['collected_by'],
+                    ]);
+                });
+                
+                // link these payments to the newly created tenant contract
+                $tenantContract->tenantPayments()->saveMany($futurePayments);
+            }
+
+            return $tenantContract;
+        });
 
         return $tenantContract;
     }

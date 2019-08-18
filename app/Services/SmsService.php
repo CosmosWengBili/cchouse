4a11fefc3d871889e02710e9c726d2480ef7ad7e
David Liu <msglight4874@gmail.com>
2019-08-18 18:31:17 +0800

    add SmsService

---
+++ app/Services/SmsService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use Mitake\Client;
+use Mitake\Message\Message;
+use Mitake\Message\Response;
+
+class SmsService {
+    public static function send(string $mobile, string $content): Response {
+        $message = (new Message())->setDstaddr($mobile)->setSmbody($content);
+        return self::client()->send($message);
+    }
+
+    private static function client() {
+        return $client = new Client(env('MITAKE_USERNAME'), env('MITAKE_PASSWORD'));
+    }
+}

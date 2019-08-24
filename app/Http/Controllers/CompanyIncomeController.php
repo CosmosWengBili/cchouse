cc79417af8347329aef7af53f887c4e2d5e762f7
David Liu <msglight4874@gmail.com>
2019-08-24 18:37:14 +0800

    Add company income store action

--- app/Http/Controllers/CompanyIncomeController.php
+++ app/Http/Controllers/CompanyIncomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\CompanyIncome;
 use App\Responser\FormDataResponser;
+use Illuminate\Http\Request;
 
 class CompanyIncomeController extends Controller
 {
@@ -12,5 +13,23 @@ class CompanyIncomeController extends Controller
         $data = $responser->create(CompanyIncome::class, 'companyIncomes.store')->get();
 
         return view('company_incomes.form', $data);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $this->fetchValidateData($request);
+        CompanyIncome::create($validatedData);
+
+        return redirect()->route('companyIncomes.index');
+    }
+
+
+    private function fetchValidateData(Request $request) {
+        return $request->validate([
+            'tenant_contract_id' => 'required',
+            'subject' => 'required',
+            'income_date' => 'required',
+            'amount' => 'required',
+            'comment' => 'required',
+        ]);
     }
 }

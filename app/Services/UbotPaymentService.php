b60ab61fee6f55cefbc50e9859a6700c304a5837
David Liu <msglight4874@gmail.com>
2019-09-29 14:32:15 +0800

    將 Secret Keys 搬到 env files

--- app/Services/UbotPaymentService.php
+++ app/Services/UbotPaymentService.php
@@ -5,30 +5,6 @@ namespace App\Services;
 use Carbon\Carbon;
 
 class UbotPaymentService {
-    public const UBOT_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1t0Exp+7O41LGd0zrWW1
-dqdTLr4YJ2qkS70O1Q7tiTFczRldW0/QevaFT9RPmjl8V9b5fwMM/YlBv+yICon0
-rAH4uGTMJJc8TbpvXtK7UbwmDPDQ4HTNWTgZmJ2feaEZn/FKOxgmMY7bowgC2Nvz
-0rWQv0ZsOlLJl3SZSuqSjpjfmmg3bXIYum1sv4TzMreOiuhctCMbdm9W1ONmJKZj
-luOXKoxiuYUt4NUEh0vRLFkD4SJAhAILe4eRXgMcaTpPJWrnWnCMXJ9LYd7BQMKn
-QKgA+uRLCBhnMz7BLL4Qygx5b+aFxRLRxxgMNAfljB008rDi/8Gh0Ph8zhtS4R/B
-JQIDAQAB
------END PUBLIC KEY-----";
-
-    // 用於產生 UBot AES Key
-    public const SECRET_KEY = "-----BEGIN SECRET KEY-----
-Wng3M29zMXhyNnNOM3YzcVhnMDlqSTVackVSb0FjWmZMUzI2Sk5GL2FGaTd6NnZT
-VzZKNFNBb0wzQ1FLQlM3c3UyKzZlVXVCMGV2N3VBa0VZa3ppc2hWTTFlVEdrdWR4
-NjZtNmRDUkIwT1FHc3NXcFdBTzhBKzZDSnBPSlZ0eUxWLzA2RHMzZnlmSVVnUXpW
-YkRqRnpYdUxjYk80RE5WcnRFRkl5cUtTOGh6UEpwY0hDMG8zQ1RHblF6M2pQUGdP
-RGRUYnVLRUUxOVFINTh3LzlQRGNnUHM1RmRkT05DQkpLekpQSklhUFJ6cnV5a0VV
-M2h1WVFHYjlmQzRjTFNqM3U3SkxkWmZrbXZ3VTNCZG1pQ0pHSEdnaVV4M2IxRWQz
-WXpkV2xMemVsSDhOOWtMcGxnd2J4WjVrTzBuQkNTcm92VVVud1VWMXdmbjQzMjMy
-SjltK093PT0=
------END SECRET KEY-----";
-
-    public const UBOT_IV = 'UBOTSECRETIVSEED';
-
     private $dataString;
     private $data;
     private $mac;
@@ -78,7 +54,7 @@ SjltK093PT0=
     }
 
     private function getSecretKeyContent() {
-        $lines = explode("\n", self::SECRET_KEY);
+        $lines = explode("\n", env('UBOT_SECRET_KEY'));
         array_shift($lines); // remove first line
         array_pop($lines);   // remove end line
 
@@ -90,7 +66,7 @@ SjltK093PT0=
     }
 
     private function getUBotPublicKey() {
-        return openssl_pkey_get_public(self::UBOT_PUBLIC_KEY);
+        return openssl_pkey_get_public(env('UBOT_PUBLIC_KEY'));
     }
 
     private function getUBotAESKey() {
@@ -109,7 +85,7 @@ SjltK093PT0=
     }
 
     private function getUBotAESIV() {
-        return self::UBOT_IV;
+        return env('UBOT_IV');
     }
 
     // 明文—SHA256--->Base64--->AES CBC/PKCS5Padding--->Base64--->mac

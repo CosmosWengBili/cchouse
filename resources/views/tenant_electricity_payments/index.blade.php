a6b88aabb8b1d5020d6fe0058eea89431eec362a
David Liu <msglight4874@gmail.com>
2019-10-06 23:07:33 +0800

    個別發送加上預覽按鈕

--- resources/views/tenant_electricity_payments/index.blade.php
+++ resources/views/tenant_electricity_payments/index.blade.php
@@ -65,6 +65,7 @@
                                                 @endif
                                             @endforeach
                                             <td>
+                                                <button class="btn btn-danger" data-toggle="modal" data-target='#send-electricity-payment-report-sms-model' data-tenant-contract-id="{{$object['id']}}">發送電費報表</button>
                                                 <a class="btn btn-success" href="{{ route( Str::camel(Str::plural($layer)) . '.show', $object['id']) }}?with=building;room;tenantPayments;tenantElectricityPayments;payLogs">查看</a>
                                                 <a class="btn btn-primary" href="{{ route( Str::camel(Str::plural($layer)) . '.edit', $object['id']) }}">編輯</a>
                                                 <a class="btn btn-danger jquery-postback" data-method="delete" href="{{ route( Str::camel(Str::plural($layer)) . '.destroy', $object['id']) }}">刪除</a>
@@ -97,7 +98,7 @@
                     <div class="form-group row">
                         <label class="col-2 col-form-label" for="sms-year">年份：</label>
                         <div class="col-10">
-                            <select name="year" id="sms-year" class="form-control form-control-sm">
+                            <select name="year" class="form-control form-control-sm">
                                 @php
                                     $now = \Carbon\Carbon::now();
                                     $currentYear = $now->year;
@@ -112,7 +113,7 @@
                     <div class="form-group row">
                         <label class="col-2 col-form-label" for="sms-month">年份：</label>
                         <div class="col-10">
-                            <select name="month" id="sms-month" class="form-control">
+                            <select name="month" class="form-control">
                                 @for ($i = 1; $i <= 12; $i++)
                                     <option value="{{ $i }}" {{ ($currentMonth == $i) ? 'selected="selected"' : '' }}>{{ $i }}</option>
                                 @endfor
@@ -130,14 +131,118 @@
 </div>
 <script>
     (function() {
-        $('#send-report-to-all-by-sms-model button.js-submit').on('click', function () {
-            const year = $('select[name="year"]').val();
-            const month = $('select[name="month"]').val();
+        var $model = $('#send-report-to-all-by-sms-model');
+        $model.find('button.js-submit').on('click', function () {
+            const year = $model.find('select[name="year"]').val();
+            const month = $model.find('select[name="month"]').val();
             const data =  { year: year, month: month };
 
             $.post("{{ route('tenantElectricityPayments.sendReportSMSToAll') }}", data, function () {
                 alert('發送成功');
-                $('#send-report-to-all-by-sms-model').modal('hide');
+                $model.modal('hide');
+                $('.modal-backdrop').remove();
+            }).fail(function () {
+                alert('發送失敗');
+            })
+        })
+    })();
+</script>
+<div class="modal fade" tabindex="-1" role="dialog" id="send-electricity-payment-report-sms-model">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">發送電費報表簡訊 - 編號: <span class="js-fill-id"></span></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">年份：</label>
+                        <div class="col-10">
+                            <select name="year" class="form-control form-control-sm">
+                                @php
+                                    $now = \Carbon\Carbon::now();
+                                    $currentYear = $now->year;
+                                    $currentMonth = $now->month;
+                                @endphp
+                                @for ($i = 0; $i < 10; $i++)
+                                    <option value="{{ $currentYear - $i }}">{{ $currentYear - $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">年份：</label>
+                        <div class="col-10">
+                            <select name="month" class="form-control">
+                                @for ($i = 1; $i <= 12; $i++)
+                                    <option value="{{ $i }}" {{ ($currentMonth == $i) ? 'selected="selected"' : '' }}>{{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">預覽</label>
+                        <div class="col-10">
+                            <div style="word-break: break-word;" class="mt-2">
+                                <a href="" target="_blank" id="preview-report-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary js-submit">送出</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    (function() {
+        var $model = $('#send-electricity-payment-report-sms-model');
+        var $link = $model.find('#preview-report-link');
+        var sampleLink = "{{ route('tenantContracts.electricityPaymentReport', ['data' => '']) }}";
+        var tenantContractId = null;
+        var year = $model.find('select[name="year"]').val();
+        var month = $model.find('select[name="month"]').val();
+
+
+        function changePreviewLink() {
+            var timestamp = Math.floor((new Date()) / 1000);
+            var data = btoa([
+                tenantContractId,
+                year,
+                month,
+                timestamp,
+            ].join('|'));
+            var url = sampleLink + '/' + data;
+
+            $link.attr('href', url).text(url);
+        }
+
+        $model.find('select[name="year"], select[name="month"]').on('change', function () {
+            year = $model.find('select[name="year"]').val();
+            month = $model.find('select[name="month"]').val();
+            changePreviewLink();
+        });
+
+        $model.on('show.bs.modal', function (event) {
+            const button = $(event.relatedTarget);
+            tenantContractId = button.data('tenant-contract-id');
+
+            $model.find('span.js-fill-id').text(tenantContractId);
+            changePreviewLink();
+        });
+
+        $model.find('button.js-submit').on('click', function () {
+            const data =  { year: year, month: month, tenantContractId: tenantContractId };
+
+            $.post("{{ route('tenantContracts.sendElectricityPaymentReportSMS') }}", data, function () {
+                alert('發送成功');
+                $model.find.modal('hide');
                 $('.modal-backdrop').remove();
             }).fail(function () {
                 alert('發送失敗');

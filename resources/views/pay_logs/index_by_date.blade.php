a56a758ce4b2c45f62131f61fc0b64d237d10028
David Liu <msglight4874@gmail.com>
2019-10-11 14:06:06 +0800

    add tabs to index pages

--- resources/views/pay_logs/index_by_date.blade.php
+++ resources/views/pay_logs/index_by_date.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col">
-            @include('pay_logs.tabs', ['by' => 'contract'])
+            @include('pay_logs.tabs', ['by' => 'date'])
             {{-- for showing multiple types of entries returned --}}
             @foreach ( $data as $type => $entries)
                 @include('pay_logs.table', ['objects' => $entries, 'layer' => $type])

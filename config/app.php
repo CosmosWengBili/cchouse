e6decc21722550c4b127d4add9442fa5287cbcda
Ian Lee <ianhomew@gmail.com>
2019-10-16 22:35:10 +0800

    Backend optimization 2. 安裝並配置 laravel-log-viewer php artisan vendor:publish \    --provider="Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider" \    --tag=views

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \App\Providers\SmsServiceProvider::class,
+
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
     ],
 
     /*

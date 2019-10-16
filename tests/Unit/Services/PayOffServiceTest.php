db6d3f1cad6688ee4e5d90ae6194087f35b0ee2f
Ian Lee <ianhomew@gmail.com>
2019-10-16 17:17:33 +0800

    Fix PayOffServiceTest

--- tests/Unit/Services/PayOffServiceTest.php
+++ tests/Unit/Services/PayOffServiceTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tests\Unit\Services;
 
+use App\Building;
+use App\Room;
 use App\Services\PayOffService;
 use App\TenantContract;
 use App\TenantElectricityPayment;
@@ -15,13 +17,20 @@ class PayOffServiceTest extends TestCase
 
     private $tenantContract;
     private $payOffDate;
+    /** @var PayOffService */
     private $payOffService;
     private $tenantElectricityPayment;
     private $tenantPayment;
+    private $building;
+    private $room;
+    private $now;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->now = now();
+
         // disable foreign key constraints
         Schema::disableForeignKeyConstraints();
 
@@ -40,6 +49,21 @@ class PayOffServiceTest extends TestCase
         $this->tenantPayment = factory(TenantPayment::class)->make(['due_time' => $this->payOffDate]);
         $this->tenantContract->tenantPayments()->save($this->tenantPayment);
 
+        factory(Building::class)->create()->each(function (Building $building) {
+            $building->rooms()->save(
+                factory(Room::class)->create()
+            );
+        });
+        $this->tenantContract->save([
+            'room_id' => Room::first()->id,
+        ]);
+
+        $this->tenantContract->building->landlordContracts()->create([
+            'commission_type' => '包租',
+            'withdrawal_revenue_distribution' => 300,
+            'commission_start_date' => $this->now->copy()->subMonth(),
+            'commission_end_date' => $this->now->copy()->addWeeks(2),
+        ]);
         $this->payOffService = new PayOffService($this->payOffDate, $this->tenantContract);
     }
 
@@ -52,23 +76,20 @@ class PayOffServiceTest extends TestCase
 
         $this->assertEquals($endDegreeOf110v, $this->tenantElectricityPayment['110v_end_degree']);
         $this->assertEquals($endDegreeOf220v, $this->tenantElectricityPayment['220v_end_degree']);
-        $this->assertEquals($fees, [
-            [
-                'subject' => '履保金',
-                'amount' => 7777,
-                'comment' => '',
-            ],
-            [
-                'subject' => '電費',
-                'amount' => -($this->tenantElectricityPayment->amount),
-                'comment' => '',
-            ],
-            [
-                'subject' => $this->tenantPayment->subject,
-                'amount' => -($this->tenantPayment->amount),
-                'comment' => $this->tenantPayment->comment,
-            ]
-        ]);
+        $this->assertEquals($fees['履保金']['amount'], 7777);
+        $this->assertEquals($fees['電費']['amount'], -($this->tenantElectricityPayment->amount));
+
+        // TODO: 這邊未來確認商業邏輯正確以後 需要把每個科目的amount計算寫清楚 總共有 包租方式 * 代管方式 = 6種 情境
+
+//        $this->assertEquals($fees['管理費']['amount'], 0);
+//        $this->assertEquals($fees['折抵管理費']['amount'], 0);
+//        $this->assertEquals($fees['清潔費']['amount'], 0);
+//        $this->assertEquals($fees['折抵清潔費']['amount'], 0);
+//        $this->assertEquals($fees['滯納金']['amount'], 0);
+//        $this->assertEquals($fees['折抵滯納金']['amount'], 0);
+//        $this->assertEquals($fees['沒收押金']['amount'], 0);
+//        $this->assertEquals($fees['點交中退盈餘分配']['amount'], 0);
+//        $this->assertEquals($fees['租金']['amount'], 0);
     }
 
     protected function tearDown(): void

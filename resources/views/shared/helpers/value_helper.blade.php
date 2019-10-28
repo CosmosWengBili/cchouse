5f022e215ed20b515e05c77b1303313b03021aad
Cosmos <cosmos-s@hotmail.com.tw>
2019-10-28 16:00:34 +0800

    🐛 Fix: [Lang] 1. cantact_info, agent 等子表格多形欄位名稱命名
    加入缺少的語系路徑

--- resources/views/shared/helpers/value_helper.blade.php
+++ resources/views/shared/helpers/value_helper.blade.php
@@ -3,6 +3,8 @@
     {{ config('enums.yes_or_no')[(int)$value] }}
 @elseif (is_array($value))
     @include('shared.show_array_in_html', ['data' => $value])
+@elseif (strpos($value,'App') === 0)
+    {{ __('model.'.explode('\\',$value)[1].'.model_name') }}
 @else
     {{ $value }}
 @endif

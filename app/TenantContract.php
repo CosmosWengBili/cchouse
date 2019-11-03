8d7433627ea6e2ffe1d5e4d4ec862e45839d94b2
David Liu <msglight4874@gmail.com>
2019-11-03 17:26:16 +0800

    主資料欄位排序『編號』、『關聯資料編號』、『科目』、『應繳時間』、『是否沖銷』、『沖銷日期』、『費用』

--- app/TenantContract.php
+++ app/TenantContract.php
@@ -139,7 +139,16 @@ class TenantContract extends Pivot implements AuditableContract
         return $this->hasMany(
             'App\TenantElectricityPayment',
             'tenant_contract_id'
-        );
+        )->select([
+            'tenant_electricity_payments.id', # 編號
+            'tenant_electricity_payments.tenant_contract_id', # 關聯資料編號
+            'tenant_electricity_payments.subject', # 科目
+            'tenant_electricity_payments.due_time', # 應繳時間
+            'tenant_electricity_payments.is_charge_off_done', # 是否沖銷
+            'tenant_electricity_payments.charge_off_date', # 沖銷日期
+            'tenant_electricity_payments.amount', # 費用,
+            'tenant_electricity_payments.*'
+        ]);
     }
 
     /**

b600c77bb88590069c6a0288917e2ef3978352fc
johnliu <johnliutw@hotmail.com>
2019-11-08 17:43:10 +0800

    update room current electricity degree when save payOff

--- app/Http/Controllers/PayOffController.php
+++ app/Http/Controllers/PayOffController.php
@@ -140,8 +140,13 @@ class PayOffController extends Controller
                 'landlord_paid' => $validatedSumsData['should_pay'],
                 'tenant_contract_id' => $tenantContract->id,
             ]);
+            $room = $tenantContract->room;
+            $room->update([
+                'current_110v' => $validatedElectricityData['final_110v'],
+                'current_220v' => $validatedElectricityData['final_220v']
+            ]);
 
-            $room_id = $tenantContract->room->id;
+            $room_id = $room->id;
             // 產生點交盈餘相關科目
             $landlordOtherSubjects = collect($validatedItemsData)
                                     ->where('subject', '點交中退盈餘分配');
@@ -221,7 +226,7 @@ class PayOffController extends Controller
             // save electricity payment
             $tenantContract->tenantElectricityPayments()->saveMany($tenantElectricityPayments);
 
-            $virtual_account = $tenantContract->room->virtual_account;
+            $virtual_account = $room->virtual_account;
 
             // 新產生的點交科目is_old=false，如果為負數，也要能產生對應的 paylog，費用等同此科目費用，但轉化為正數 ;
             $allPayments = $tenantPayments->merge($tenantElectricityPayments);

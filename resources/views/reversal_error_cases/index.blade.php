407a3c2266261a74f799aec6780252d6c57afe99
David Liu <msglight4874@gmail.com>
2019-11-09 22:46:05 +0800

    沖銷異常管理 1. 加一個 tab，顯示所有 loggable_type 不等於 tenant_payment 和 tenant_electricity_payment, deposit 的 pay_log，並顯示“無所屬資料繳款紀錄” 2. 每一列，如果該案件連到的 paylog 的 loggable_type 是  tenant_payment 或tenant_electricity_payment，顯示一按鈕『查看』，點入後會連到該 pay_log 所屬的 tenantContract 查看頁，with 帶 room, tenant_payment, tenant_electricity_payment 和 pay_logs

--- resources/views/reversal_error_cases/index.blade.php
+++ resources/views/reversal_error_cases/index.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
 <div class="container">
-
+    <div class="row">
+        <div class="col">
+            <div class="d-flex justify-content-center">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link{{ $type != 'other' ? ' active' : '' }}" href="{{ route('reversalErrorCases.index') }}">主資料</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link{{ $type == 'other' ? ' active' : '' }}" href="{{ route('reversalErrorCases.index', ['type' => 'other']) }}">無所屬資料繳款紀錄</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-10">
-
             {{-- for showing multiple types of entries returned --}}
             @foreach ( $data as $type => $entries)
-
-
                 @include('reversal_error_cases.table', ['objects' => $entries, 'layer' => $type])
-
             @endforeach
-
         </div>
 
     </div>

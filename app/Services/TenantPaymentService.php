aad80aa97901b0c0746c712ba96d972c4b200aeb
David Liu <msglight4874@gmail.com>
2019-11-10 16:22:56 +0800

    修左邊按應繳日期，越舊越上方，而且目前有 bug，電費會分開來排序

--- app/Services/TenantPaymentService.php
+++ app/Services/TenantPaymentService.php
@@ -15,14 +15,14 @@ class TenantPaymentService
         $rows = [];
         $tenantPayments = TenantPayment::whereIn('tenant_contract_id', $tenantContractIds)
                             ->whereBetween('due_time', [$startDate, $endDate])
-                            ->get()
-                            ->sortBy(function ($tp) use ($subjects) {
-                                // 資料按 `日期` 再按 `沖銷順序` 排序
-                                return $tp->due_time . '#' . ($subjects->search($tp->subject) + 10);
-                            });
+                            ->get();
         $tenantElectricityPayments = TenantElectricityPayment::whereIn('tenant_contract_id', $tenantContractIds)
                                         ->whereBetween('due_time', [$startDate, $endDate])->get();
-        $payments = $tenantPayments->concat($tenantElectricityPayments);
+        $payments = $tenantPayments->concat($tenantElectricityPayments)
+                                   ->sortBy(function ($tp) use ($subjects) {
+                                       // 資料按 `應繳日期` 再按 `沖銷順序` 排序
+                                       return $tp->due_time . '#' . ($subjects->search($tp->subject) + 10);
+                                   });;
 
         foreach ($payments as $payment) {
             $rows[] = [

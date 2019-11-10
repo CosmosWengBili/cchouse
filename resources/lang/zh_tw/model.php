c6dd37e14bcfe58506f7db9e94f15a4ec0098c32
David Liu <msglight4874@gmail.com>
2019-11-10 16:37:44 +0800

    目前餘額改為目前欠費

--- resources/lang/zh_tw/model.php
+++ resources/lang/zh_tw/model.php
@@ -405,7 +405,7 @@ return [
         'created_at'                          => '建立時間',
         'updated_at'                          => '更新時間',
         'commissioner_id'                     => '專員編號',
-        'currentBalance'                      => '目前餘額',
+        'currentBalance'                      => '目前欠費',
         'building'                            => '物件',
         'room'                                => '房',
         'rooms'                               => '房',

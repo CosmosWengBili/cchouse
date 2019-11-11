7ba8184923b61174c5bcda79ae391f5127bfa8f3
Cosmos <cosmosweng@bili.com.tw>
2019-11-11 09:47:48 +0800

    🎨 Style: [CSS] 修改 content 寬度

--- resources/views/pay_offs/index.blade.php
+++ resources/views/pay_offs/index.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
+<div class="content-wrapper">
     <div class="row justify-content-center">
         <div class="col">
             {{-- for showing multiple types of entries returned --}}
             @foreach ( $data as $type => $entries)
-                @include('pay_offs.table', ['objects' => $entries, 'layer' => $type])
+            @include('pay_offs.table', ['objects' => $entries, 'layer' => $type])
             @endforeach
         </div>
     </div>
